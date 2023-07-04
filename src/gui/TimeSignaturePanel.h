fbe94693e1c3d26e07a7aa1d183692a8e63b2aa6
Alexandr Zyurkalov <Alexander.Zyurkalov@gmail.com>
2023-07-04 14:31:22 +0200

    Controls for time signature

--- src/gui/TimeSignaturePanel.h
+++ src/gui/TimeSignaturePanel.h
@@ -1,10 +1,8 @@
 //
 // Created by Alexandr Zyurkalov on 04.07.23.
 //
-
 #ifndef PRACTICEPLAYINGINDAW_TIMESIGNATUREPANEL_H
 #define PRACTICEPLAYINGINDAW_TIMESIGNATUREPANEL_H
-
 
 #include <juce_gui_basics/juce_gui_basics.h>
 
@@ -13,22 +11,22 @@ class TimeSignaturePanel : public juce::Component
 public:
     TimeSignaturePanel()
     {
-        addAndMakeVisible(timeSigLabel);
-        timeSigLabel.setText("Time Signature:", juce::dontSendNotification);
-        timeSigLabel.attachToComponent(&timeSigBox, true);
+        addAndMakeVisible(timeSigNumeratorBox);
+        timeSigNumeratorBox.setRange(1, 64, 1);
+        timeSigNumeratorBox.setValue(4);
+        timeSigNumeratorBox.onValueChange = [this] { timeSignatureChanged(); };
 
-        addAndMakeVisible(timeSigBox);
-        timeSigBox.addItem("2/4", 1);
-        timeSigBox.addItem("3/4", 2);
-        timeSigBox.addItem("4/4", 3);
-        timeSigBox.onChange = [this] { timeSignatureChanged(); };
-
-        addAndMakeVisible(numBarsLabel);
-        numBarsLabel.setText("Number of Bars:", juce::dontSendNotification);
-        numBarsLabel.attachToComponent(&numBarsBox, true);
+        addAndMakeVisible(timeSigDenominatorBox);
+        timeSigDenominatorBox.addItem("1", 1);
+        timeSigDenominatorBox.addItem("2", 2);
+        timeSigDenominatorBox.addItem("4", 3);
+        timeSigDenominatorBox.addItem("8", 4);
+        timeSigDenominatorBox.addItem("16", 5);
+        timeSigDenominatorBox.addItem("32", 6);
+        timeSigDenominatorBox.addItem("64", 7);
+        timeSigDenominatorBox.onChange = [this] { timeSignatureChanged(); };
 
         addAndMakeVisible(numBarsBox);
-        numBarsBox.setSliderStyle(juce::Slider::IncDecButtons);
         numBarsBox.setRange(1, 64, 1);
         numBarsBox.setValue(4);
         numBarsBox.onValueChange = [this] { numBarsChanged(); };
@@ -36,11 +34,17 @@ public:
 
     void resized() override
     {
-        auto area = getLocalBounds();
-        timeSigBox.setBounds(area.removeFromTop(25));
-        numBarsBox.setBounds(area.removeFromTop(25));
+        juce::Grid grid;
 
+        grid.templateRows    = { juce::Grid::TrackInfo(juce::Grid::Fr(1)) };
+        grid.templateColumns = {
+                juce::Grid::TrackInfo(juce::Grid::Fr(1)),
+                juce::Grid::TrackInfo(juce::Grid::Px(200)),
+                juce::Grid::TrackInfo(juce::Grid::Fr(1))
+        };
 
+        grid.items = { juce::GridItem(timeSigNumeratorBox), juce::GridItem(timeSigDenominatorBox), juce::GridItem(numBarsBox) };
+        grid.performLayout(getLocalBounds());
     }
 
     void timeSignatureChanged()
@@ -55,11 +59,11 @@ public:
 
 private:
     juce::Label timeSigLabel;
-    juce::ComboBox timeSigBox;
-
     juce::Label numBarsLabel;
+
+    juce::Slider timeSigNumeratorBox;
+    juce::ComboBox timeSigDenominatorBox;
     juce::Slider numBarsBox;
 };
-
 
 #endif //PRACTICEPLAYINGINDAW_TIMESIGNATUREPANEL_H
