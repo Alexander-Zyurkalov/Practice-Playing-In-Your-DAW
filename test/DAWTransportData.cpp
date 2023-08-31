950ee402fc3c9f6421ffdb4010a0e4bc36c95ee1
Alexandr Zyurkalov <Alexander.Zyurkalov@gmail.com>
2023-08-31 20:26:55 +0200

    more test cases for numberOfBars

--- test/DAWTransportData.cpp
+++ test/DAWTransportData.cpp
@@ -8,19 +8,29 @@
 TEST_CASE("numberOfBars", "[DAWTransportData]")
 {
 
-    DAWTransportData dawTransportData{4, 4};
-    std::vector<double> barPPQPositions{0, 4, 8, 12, 16, 20, 24, 28, 32};
-    int numberOfBars = 8;
-    for (int i = 0; i < numberOfBars; ++i)
-    {
-        dawTransportData.set(0, 0, barPPQPositions[i]);
-        REQUIRE(dawTransportData.getNumBars() == i);
-    }
+    std::vector<std::pair<DAWTransportData,std::vector<double>>> measures{
+            {DAWTransportData{4, 4}, {0, 4, 8, 12, 16, 20, 24, 28, 32}},
+            {DAWTransportData{3, 4}, {0, 3, 6, 9, 12, 15, 18, 21, 24}},
+            {DAWTransportData{5, 4}, {0, 5, 10, 15, 20, 25, 30, 35, 40}},
+            {DAWTransportData{3, 8}, {0, 1.5, 3, 4.5, 6, 7.5, 9, 10.5, 12}},
+    };
 
-    for (int i = 1; i < numberOfBars; ++i)
+    for (auto& measure : measures)
     {
-        dawTransportData.set(0, 1, barPPQPositions[i]);
-        REQUIRE(dawTransportData.getNumBars() == i-1);
-    }
+        DAWTransportData& dawTransportData = measure.first;
+        std::vector<double>& barPPQPositions = measure.second;
+        int numberOfBars = static_cast<int>(barPPQPositions.size()) - 1;
 
+        for (int i = 0; i < numberOfBars; ++i)
+        {
+            dawTransportData.set(0, 0, barPPQPositions[i]);
+            REQUIRE(dawTransportData.getNumBars() == i);
+        }
+
+        for (int i = 1; i < numberOfBars; ++i)
+        {
+            dawTransportData.set(0, 1, barPPQPositions[i]);
+            REQUIRE(dawTransportData.getNumBars() == i - 1);
+        }
+    }
 }
