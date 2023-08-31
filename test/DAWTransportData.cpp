84a6f9e63d5d503135f45719c07af280a073c39b
Alexandr Zyurkalov <Alexander.Zyurkalov@gmail.com>
2023-08-31 20:07:10 +0200

    testing numberNumberOfBars

---
+++ test/DAWTransportData.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Alexandr Zyurkalov on 31.08.23.
+//
+
+#include "catch2/catch_test_macros.hpp"
+#include "../src/processing/DAWTransportData.h"
+
+TEST_CASE("numberOfBars", "[DAWTransportData]")
+{
+
+    DAWTransportData dawTransportData{4, 4};
+    std::vector<double> barPPQPositions{0, 4, 8, 12, 16, 20, 24, 28, 32};
+    int numberOfBars = 8;
+    for (int i = 0; i < numberOfBars; ++i)
+    {
+        dawTransportData.set(0, 0, barPPQPositions[i]);
+        REQUIRE(dawTransportData.getNumBars() == i);
+    }
+
+}
