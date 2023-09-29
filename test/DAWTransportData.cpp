274a12becb7801008f8876aa84b436bc5c07b1e4
Alexandr Zyurkalov <Alexander.Zyurkalov@gmail.com>
2023-09-29 10:48:41 +0200

    another test case

--- test/DAWTransportData.cpp
+++ test/DAWTransportData.cpp
@@ -52,15 +52,22 @@ TEST_CASE("timeSignatures", "[DAWTransportData]")
                     .beats{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16},
                     .bars{0, 4, 8, 12, 16, 20, 24}
             },
-//            {
-//                    .dawTransportData{4,4},
-//                    .timeSignatureChanges{
-//                            {8.0, TimeSignature{4, 8}},
-//                    },
-//                    .beats{0, 1, 2, 3, 4, 5, 6, 7,
-//                           8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14, 14.5, 15, 15.5, 16},
-//                    .bars{0, 4, 8, 10, 12, 14, 16}
-//            },
+            { // here we don't see any shift in the beats and bars
+                    .dawTransportData{4,4},
+                    .timeSignatureChanges{
+                            {8.0, TimeSignature{4, 4}},
+                    },
+                    .beats{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16},
+                    .bars{0, 4, 8, 12, 16, 20, 24}
+            },
+            { // here we will see a shift of 0.5 beats for bars and beats
+                .dawTransportData{4,4},
+                .timeSignatureChanges{
+                        {0.5, TimeSignature{4, 4}},
+                },
+                .beats{0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5, 10.5, 11.5, 12.5, 13.5, 14.5, 15.5, 16.5},
+                .bars{0.5, 4.5, 8.5, 12.5, 16.5, 20.5, 24.5}
+            }
 
     };
 
