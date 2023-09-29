d03d5eeb2129e409f874ebe724b1f628daa2cd90
Alexandr Zyurkalov <Alexander.Zyurkalov@gmail.com>
2023-09-29 10:42:45 +0200

    simplifies the test a litle bit

--- test/DAWTransportData.cpp
+++ test/DAWTransportData.cpp
@@ -40,7 +40,6 @@ struct TimeSignaturesTestRecord
     DAWTransportData dawTransportData;
     std::vector<std::pair<double, TimeSignature>> timeSignatureChanges;
     std::vector<double> beats;
-    std::vector<double> barsForTheInitialTimeSignature;
     std::vector<double> bars;
 };
 
@@ -51,7 +50,6 @@ TEST_CASE("timeSignatures", "[DAWTransportData]")
                     .dawTransportData{4,4},
                     .timeSignatureChanges{},
                     .beats{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16},
-                    .barsForTheInitialTimeSignature{0, 4, 8, 12, 16, 20, 24},
                     .bars{0, 4, 8, 12, 16, 20, 24}
             },
 //            {
@@ -61,38 +59,13 @@ TEST_CASE("timeSignatures", "[DAWTransportData]")
 //                    },
 //                    .beats{0, 1, 2, 3, 4, 5, 6, 7,
 //                           8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14, 14.5, 15, 15.5, 16},
-//                    .barsForTheInitialTimeSignature{0, 4, 8, 12, 16},
 //                    .bars{0, 4, 8, 10, 12, 14, 16}
 //            },
-//            {
-//                    .dawTransportData{4,4},
-//                    .timeSignatureChanges{
-//                            {7.0, TimeSignature{4, 8}},
-//                    },
-//                    .beats{0, 1, 2, 3, 4, 5, 6, 7,
-//                           7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14, 14.5, 15},
-//                    .barsForTheInitialTimeSignature{0, 4, 8, 12, 16},
-//                    .bars{0, 4, 7, 9, 11, 13, 15}
-//            },
+
     };
 
     for (TimeSignaturesTestRecord& timeSignatureTestRecord: timeSignatureTestRecords)
     {
-        // setting the initial loop
-        timeSignatureTestRecord.dawTransportData.setLoop(0.0,
-                                                         timeSignatureTestRecord.barsForTheInitialTimeSignature.front(),
-                                                         timeSignatureTestRecord.barsForTheInitialTimeSignature.back());
-
-        // checking the initial bars
-        size_t numberOfBars = timeSignatureTestRecord.barsForTheInitialTimeSignature.size();
-        for (size_t i = 0; i < numberOfBars; ++i)
-        {
-            REQUIRE(
-                timeSignatureTestRecord.dawTransportData.getBarPosition(i) ==
-                    timeSignatureTestRecord.barsForTheInitialTimeSignature[i]
-            );
-        }
-
         // checking the initial time signature
         double ppq=timeSignatureTestRecord.dawTransportData.getPpqStartLoopPosition();
         while (ppq<timeSignatureTestRecord.dawTransportData.getPpqEndLoopPosition())
